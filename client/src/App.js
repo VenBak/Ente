514ae032ab65189854aa6631f6cd28519d006bc4
VenBak <andre.brahin@gmail.com>
2024-05-06 20:41:50 +0200

    essential buttons

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>
+          Login
+        </button>
+      </header>
+      <main>
+        <button>
+          Play
+        </button>
+        <button>
+          Rules
+        </button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
